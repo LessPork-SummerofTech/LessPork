f408d688f35220b247db29ad3538e5639531a6bd
ZilvaX <Kevin.x.g@gmail.com>
2015-08-09 08:59:14 +1200

    Changes to feedback
    Added the console logs for checking rating and comments

--- LikeABus/LikeABus/Scripts/LikeABus/feedback.js
+++ LikeABus/LikeABus/Scripts/LikeABus/feedback.js
@@ -1,12 +1,23 @@
-﻿$(document).ready(submitForm);
-
+﻿$(document).ready(submitForm());
+/*
+function person(rating, comment) {
+    this.rating = rating;
+    this.comment = comment;
+}
+*/
 function submitForm() {
     console.log("opened script file");
-    $("#submit").click(function() {
-        if ($('input[name=gender]:checked').length > 0) {
-            debugger;
-            console.log("is selected");
-        }
-    });
-    
 }
+function myFunction() {
+    console.log("Hello myFunction()s")
+    if (document.getElementById('up').checked) {
+        console.log("UP");
+    }
+    if (document.getElementById('down').checked) {
+        console.log("DOWN");
+    }
+    console.log(document.getElementById('textArea').value);
+}
+
+
+
